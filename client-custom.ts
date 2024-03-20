880a7f0f15d68e0845f69da4904e30e8d6fe91d0
eddiechayes <eddiechayes@gmail.com>
2024-03-20 11:25:06 -0700

    Generate Visier typescript SDK

---
+++ client-custom.ts
@@ -0,0 +1,5 @@
+import { ConfigurationParameters } from "./configuration";
+
+export class VisierDocumentSearchCustom {
+    constructor(configurationParameters: ConfigurationParameters) {}
+}
---
+++ index.test.ts
@@ -0,0 +1,8 @@
+import { VisierDocumentSearch } from "./index";
+
+describe("visier-document-search-typescript-sdk", () => {
+    it("initialize client", async () => {
+        const visierdocumentsearch = new VisierDocumentSearch({
+        });
+    });
+});
